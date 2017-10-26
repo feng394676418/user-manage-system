cd679546e08989e069047ee4655995be1cef0c06
lvwd <863128292@qq.com>
2017-10-26 17:06:43 +0800

    new add

--- src/api/order.js
+++ src/api/order.js
@@ -10,8 +10,16 @@ export function orderInfoCreate(form) {
 
 export function getOrderByRefnumber(refnumber) {
     return fetch({
-        url: 'api/order/getOrderByRefnumber',
+        url: 'api/apply/getOrderByRefnumber',
         method: 'get',
         params: { refnumber }
     })
+}
+
+export function getRouterLog(refnumber) {
+  return fetch({
+      url: 'api/apply/getRouterLog',
+      method: 'get',
+      params: { refnumber }
+  })
 }
