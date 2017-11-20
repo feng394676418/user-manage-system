301c18a20794dec165d10f3b4a3c82c0b56cfea1
LiSyd <lixiaode621@163.com>
2017-11-20 17:09:34 +0800

    支付成功页面金额保留两位小数

--- src/components/ApplyAss/PaymentSuccessful.vue
+++ src/components/ApplyAss/PaymentSuccessful.vue
@@ -27,7 +27,7 @@
                 <tr>
                     <td>{{serialnumber}}</td>
                     <td>{{billType}}</td>
-                    <td>€ <strong class="Orange_text">{{totalCost}}</strong></td>
+                    <td>€ <strong class="Orange_text">{{totalCost | money}}</strong></td>
                 </tr>
             </table>
              <div class="col-md-12 text-center">
@@ -75,6 +75,26 @@ export default {
             orderNumber: this.$route.query.nber
           }
         }
+    },
+    filters: {
+        money(val) {
+            val = val.toString().replace(/\$|,/g, '')
+            if (isNaN(val)) {
+            val = '0'
+            }
+            const sign = val === (val = Math.abs(val))
+            val = Math.floor(val * 100 + 0.50000000001)
+            let cents = val % 100
+            val = Math.floor(val / 100).toString()
+            if (cents < 10) {
+            cents = '0' + cents
+            }
+            for (let i = 0; i < Math.floor((val.length - (1 + i)) / 3); i++) {
+                val = val.substring(0, val.length - (4 * i + 3)) + ',' + val.substring(val.length - (4 * i + 3))
+            }
+
+            return sign ? '' : val + '.' + cents
+            }
     },
     created() {
       this.validatePay()
