87c62c3adbc6bdd4dc09033c7c25711bf904907d
liuwy <731839038@qq.com>
2017-12-28 11:14:07 +0800

    update

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -35,7 +35,7 @@ var webpackConfig = merge(baseWebpackConfig, {
             compress: {
                 warnings: false
             },
-            sourceMap: true
+            //sourceMap: true
         }),
         // extract css into its own file
         new ExtractTextPlugin({
